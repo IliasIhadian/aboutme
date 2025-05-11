4ee596bded1b6d9d8cc324c239d1d88f16504959
Ilias Ihadian <i.ihadian@gmail.com>
2025-05-11 15:38:27 +0200

    fix

--- src/app/api/pixel/route.ts
+++ src/app/api/pixel/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest } from "next/server";
 import nodemailer from "nodemailer";
+import { secret } from '@aws-amplify/backend';
 export const dynamic = 'force-dynamic'; // ← wichtig!
 
 
@@ -10,8 +11,8 @@ export async function GET(req: NextRequest) {
 
   console.log("📥 API aufgerufen");
 
-    console.log("🔐 GMAIL_APP_USER:", process.env.GMAIL_APP_USER ? "gesetzt" : "leer");
-    console.log("🔐 GMAIL_APP_PASSWORD:", process.env.GMAIL_APP_PASSWORD ? "gesetzt" : "leer");
+    console.log("🔐 GMAIL_APP_USER:", secret(GMAIL_APP_USER) ? "gesetzt" : "leer");
+    console.log("🔐 GMAIL_APP_PASSWORD:", secret(GMAIL_APP_PASSWORD) ? "gesetzt" : "leer");
 
   // ✅ Mailer einrichten (mit AWS SES oder SMTP)
   const transporter = nodemailer.createTransport({
